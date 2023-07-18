128bc478b9dbcb3171bb1f960f2907d42552f4c1
infotecbd <mdhossain.odesk@gmail.com>
2023-07-19 01:25:10 +0600

    pos

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Helper\JWTToken;
+use App\Mail\OTPMail;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    function UserRegistration(Request $request){
+
+        try {
+            User::create([
+                'firstName' => $request->input('firstName'),
+                'lastName' => $request->input('lastName'),
+                'email' => $request->input('email'),
+                'mobile' => $request->input('mobile'),
+                'password' => $request->input('password'),
+            ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successfully'
+            ],200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'User Registration Failed'
+            ],200);
+
+        }
+    }
+}
