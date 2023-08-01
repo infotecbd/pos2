29b6d9d67d384869f494b9cad5e0204fdea45607
infotecbd <mdhossain.odesk@gmail.com>
2023-08-01 23:17:02 +0600

    update

--- app/Helper/JWTToken.php
+++ app/Helper/JWTToken.php
@@ -1,43 +1,59 @@
 <?php
+
 namespace App\Helper;
+
 use Exception;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+//use Lcobucci\JWT\Parser;
+//use Lcobucci\JWT\ValidationData;
+//use Lcobucci\JWT\Signer\Hmac\Sha256;
 
 class JWTToken
 {
-    public static function CreateToken($userEmail) {
 
+    public static function CreateToken($userEmail,$userID):string{
         $key =env('JWT_KEY');
         $payload=[
             'iss'=>'laravel-token',
             'iat'=>time(),
             'exp'=>time()+60*60,
             'userEmail'=>$userEmail,
-
+            'userID'=>$userID
         ];
        return JWT::encode($payload,$key,'HS256');
     }
 
 
-
-   public static function VerifyToken($token):string
-    {
-        try{
-            $key = env('JWT_KEY');
-            $decode = JWT::decode($token, new Key($key, 'HS256'));
-            return $decode->userEmail;
-
-        }
-
-        catch (Exception $e){
-            return 'Unauthorized';
-        }
-
-
+    public static function CreateTokenForSetPassword($userEmail):string{
+        $key =env('JWT_KEY');
+        $payload=[
+            'iss'=>'laravel-token',
+            'iat'=>time(),
+            'exp'=>time()+60*20,
+            'userEmail'=>$userEmail,
+            'userID'=>'0'
+        ];
+        return JWT::encode($payload,$key,'HS256');
     }
+
+
+
+    public static function VerifyToken($token):string|object
+    {
+        try {
+            if($token==null){
+                return 'unauthorized';
+            }
+            else{
+                $key =env('JWT_KEY');
+                $decode=JWT::decode($token,new Key($key,'HS256'));
+                return $decode;
+            }
+        }
+        catch (Exception $e){
+            return 'unauthorized';
+        }
+    }
+
 }
-
-
-
-?>
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 use App\Helper\JWTToken;
 use App\Mail\OTPMail;
@@ -11,10 +10,34 @@ use Illuminate\View\View;
 
 class UserController extends Controller
 {
-    function UserRegistration(Request $request){
 
+    function LoginPage():View{
+        return view('pages.auth.login-page');
+    }
+
+    function RegistrationPage():View{
+        return view('pages.auth.registration-page');
+    }
+    function SendOtpPage():View{
+        return view('pages.auth.send-otp-page');
+    }
+    function VerifyOTPPage():View{
+        return view('pages.auth.verify-otp-page');
+    }
+
+    function ResetPasswordPage():View{
+        return view('pages.auth.reset-pass-page');
+    }
+
+    function ProfilePage():View{
+        return view('pages.dashboard.profile-page');
+    }
+
+
+
+    function UserRegistration(Request $request){
         try {
-              User::create([
+            User::create([
                 'firstName' => $request->input('firstName'),
                 'lastName' => $request->input('lastName'),
                 'email' => $request->input('email'),
@@ -36,28 +59,138 @@ class UserController extends Controller
     }
 
     function UserLogin(Request $request){
-        $count= User::where('email', '=', $request->input('email'))
-        ->where('password', '=', $request->input('password'))->count();
 
-        if($count==1) {
-            // User Login-> JWT Token issue korbo
+       $count=User::where('email','=',$request->input('email'))
+            ->where('password','=',$request->input('password'))
+            ->select('id')->first();
 
-            $token = JWTToken::CreateToken($request->input('email'));
+
+       if($count!==null){
+           // User Login-> JWT Token Issue
+           $token=JWTToken::CreateToken($request->input('email'),$count->id);
+           return response()->json([
+               'status' => 'success',
+               'message' => 'User Login Successful',
+           ],200)->cookie('token',$token,60*24*30);
+       }
+       else{
+           return response()->json([
+               'status' => 'failed',
+               'message' => 'unauthorized'
+           ],200);
+
+       }
+
+    }
+
+    function SendOTPCode(Request $request){
+
+        $email=$request->input('email');
+        $otp=rand(1000,9999);
+        $count=User::where('email','=',$email)->count();
+
+        if($count==1){
+            // OTP Email Address
+            Mail::to($email)->send(new OTPMail($otp));
+            // OTO Code Table Update
+            User::where('email','=',$email)->update(['otp'=>$otp]);
 
             return response()->json([
-                'status' => 'Success',
-                'message' => 'User Authorized'
-            ], 200);
+                'status' => 'success',
+                'message' => '4 Digit OTP Code has been send to your email !'
+            ],200);
         }
-        else {
+        else{
             return response()->json([
                 'status' => 'failed',
-                'message' => 'unauthorized',
-                'token' => $token,
-            ], 200);
-
-
-
+                'message' => 'unauthorized'
+            ]);
         }
     }
+
+    function VerifyOTP(Request $request){
+        $email=$request->input('email');
+        $otp=$request->input('otp');
+        $count=User::where('email','=',$email)
+            ->where('otp','=',$otp)->count();
+
+        if($count==1){
+            // Database OTP Update
+            User::where('email','=',$email)->update(['otp'=>'0']);
+
+            // Pass Reset Token Issue
+            $token=JWTToken::CreateTokenForSetPassword($request->input('email'));
+            return response()->json([
+                'status' => 'success',
+                'message' => 'OTP Verification Successful',
+            ],200)->cookie('token',$token,60*24*30);
+
+        }
+        else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'unauthorized'
+            ],200);
+        }
+    }
+
+    function ResetPassword(Request $request){
+        try{
+            $email=$request->header('email');
+            $password=$request->input('password');
+            User::where('email','=',$email)->update(['password'=>$password]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Request Successful',
+            ],200);
+
+        }catch (Exception $exception){
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Something Went Wrong',
+            ],200);
+        }
+    }
+
+    function UserLogout(){
+        return redirect('/userLogin')->cookie('token','',-1);
+    }
+
+
+    function UserProfile(Request $request){
+        $email=$request->header('email');
+        $user=User::where('email','=',$email)->first();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Request Successful',
+            'data' => $user
+        ],200);
+    }
+
+    function UpdateProfile(Request $request){
+        try{
+            $email=$request->header('email');
+            $firstName=$request->input('firstName');
+            $lastName=$request->input('lastName');
+            $mobile=$request->input('mobile');
+            $password=$request->input('password');
+            User::where('email','=',$email)->update([
+                'firstName'=>$firstName,
+                'lastName'=>$lastName,
+                'mobile'=>$mobile,
+                'password'=>$password
+            ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Request Successful',
+            ],200);
+
+        }catch (Exception $exception){
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Something Went Wrong',
+            ],200);
+        }
+    }
+
 }
